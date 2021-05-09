e0ddced9a6907308d36e9e7704664230c1864861
Shubham Rajput <shubham.rajput0369@gmail.com>
2021-05-09 23:46:50 +0530

    Update 1.6 - Java Loops I.java

--- 1.6 - Java Loops I.java
+++ 1.6 - Java Loops I.java
@@ -11,3 +11,6 @@ import java.util.regex.*;
 
 public class Solution {
     private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
