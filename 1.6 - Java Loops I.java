a0fe64af078b946c59f39db778984436ae0ffc47
Shubham Rajput <shubham.rajput0369@gmail.com>
2021-05-09 23:47:20 +0530

    Update 1.6 - Java Loops I.java

--- 1.6 - Java Loops I.java
+++ 1.6 - Java Loops I.java
@@ -15,3 +15,4 @@ public class Solution {
     public static void main(String[] args) {
         int N = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        for(int i=1;i<=10;i++)
