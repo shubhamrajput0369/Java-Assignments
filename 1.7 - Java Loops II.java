6b002a9397b337badcf59ec0f358dfdcff73e9c3
Shubham Rajput <shubham.rajput0369@gmail.com>
2021-05-09 23:51:56 +0530

    Update 1.7 - Java Loops II.java

--- 1.7 - Java Loops II.java
+++ 1.7 - Java Loops II.java
@@ -10,3 +10,5 @@ class Solution{
         Scanner in = new Scanner(System.in);
         int t=in.nextInt();
         for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
